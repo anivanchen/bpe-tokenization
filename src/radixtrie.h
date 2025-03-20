05f4e7170904a27e416218047ac5ff88dbf13b40
Ivan Chen <ivanchen07@gmail.com>
2025-03-20 11:04:47 -0400

    Add helper functions and init root node

--- src/radixtrie.h
+++ src/radixtrie.h
@@ -14,4 +14,29 @@ class Node {
   Node() : key(""), isEnd(false) {}
 };
 
+class RadixTrie {
+ private:
+  Node* root;
+
+  // Recursively delete child nodes
+  void deleteSubTree(Node* node) {
+    if (node == nullptr) return;
+    for (auto& child : node->children) deleteSubTree(child.second);
+    delete node;
+  }
+
+  // Print the Trie recursively
+  void printHelper(Node* node, const std::string& prefix) {
+    if (node->isEnd) std::cout << prefix + " " + node->key << std::endl;
+    for (auto& child : node->children) printHelper(child.second, prefix + child.first);
+  }
+
+ public:
+  RadixTrie() { root = new Node(); }
+  ~RadixTrie() { deleteSubTree(root); }
+
+  // Utility function to print the Radix Trie
+  void print() { printHelper(root, ""); }
+};
+
 #endif
