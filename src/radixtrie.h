1e1e4f62fcd38bc5dd2ed9bae88c20eeae763761
Ivan Chen <ivanchen07@gmail.com>
2025-03-20 11:03:01 -0400

    Implement Node class

--- src/radixtrie.h
+++ src/radixtrie.h
@@ -4,6 +4,14 @@
 
 #include "main.h"
 
+class Node {
+ public:
+  std::string key;
+  std::unordered_map<char, Node*> children;
+  bool isEnd;
 
+  Node(std::string key) : key(key), isEnd(false) {}
+  Node() : key(""), isEnd(false) {}
+};
 
 #endif
